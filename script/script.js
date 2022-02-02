d52453559e280f88e618ff80061fa175a8f7c9a1
yana <yanags1990@gmail.com>
2022-02-02 14:53:36 +0200

    Slider settigns

--- script/script.js
+++ script/script.js
@@ -13,9 +13,10 @@ $(document).ready(function() {
 
     $('.info__slider').slick({
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 2,
+        slidesToScroll: 1,
         arrows: true,
+        centerMode: true,
         nextArrow: '<button class="slide-arrow prev-arrow"><img src="img/arrow-right-side.png" alt="arrow-right"></button>',
         prevArrow: '<button class="slide-arrow next-arrow"><img src="img/arow-left-side.png" alt="arrow-left"></button>'
     });
